9531535b86534a15932dfe1d19f76d3363b55f7f
Christopher Probst-Ranly <christopher@knockturn.io>
2025-09-10 00:23:01 +0200

    Add new queue notification feature (#432)
    * Add new queue notification feature that notifies listeners about inserts, updates and deletions of messages. This is opt-in and be enable/disabled on a per-queue basis. This feature requires client support as LISTEN/NOTIFY alone do not help much. This is a starting point to allow real-time updates additionally to polling.
    
    * Add schema and OP to notification channel name. Use NULL as payload. Reason: If the OP is payload, the client-side has to deal with differentiating which OP happened and it also causes more traffic as every listener is notified about everything.
    
    * Restrict to only notify INSERT events and use ::regclass to prevent injection.
    
    * Move pgmq. into string construction to make ::regclass work correctly.
    
    * use pgmq.format_table_name() for safety and consistency
    
    * Bump minor version and add migration script.
    
    * Add IF EXISTS to DROP TRIGGER so that enable/disable notifications are idempotent.
    
    ---------
    
    Co-authored-by: Adam Hendel <hendel.adam@gmail.com>

--- pgmq-extension/sql/pgmq.sql
+++ pgmq-extension/sql/pgmq.sql
@@ -1125,3 +1125,44 @@ BEGIN
   );
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION pgmq.notify_queue_listeners()
+RETURNS TRIGGER AS $$
+BEGIN
+  PERFORM PG_NOTIFY('pgmq.' || TG_TABLE_NAME || '.' || TG_OP, NULL);
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION pgmq.enable_notify_insert(queue_name TEXT)
+RETURNS void AS $$
+DECLARE
+  qtable TEXT := pgmq.format_table_name(queue_name, 'q');
+BEGIN
+  PERFORM pgmq.disable_notify_insert(queue_name);
+  EXECUTE FORMAT(
+    $QUERY$
+    CREATE CONSTRAINT TRIGGER trigger_notify_queue_insert_listeners
+    AFTER INSERT ON pgmq.%I
+    DEFERRABLE FOR EACH ROW
+    EXECUTE PROCEDURE pgmq.notify_queue_listeners()
+    $QUERY$,
+    qtable
+  );
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION pgmq.disable_notify_insert(queue_name TEXT)
+RETURNS void AS $$
+DECLARE
+  qtable TEXT := pgmq.format_table_name(queue_name, 'q');
+BEGIN
+  EXECUTE FORMAT(
+    $QUERY$
+    DROP TRIGGER IF EXISTS trigger_notify_queue_insert_listeners ON pgmq.%I;
+    $QUERY$,
+    qtable
+  );
+END;
+$$ LANGUAGE plpgsql;
+
